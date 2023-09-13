1eabfddf8ad7d57dd38d7f0680b214719c60909f
John Campbell <campbj49@gmail.com>
2023-09-13 12:55:31 -0400

    data is displaying, but not formatted

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -1,7 +1,11 @@
 const PokeCard = (props)=>{
+    let imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`
     return(
-        <div id={props.id}>
-            <p>The name of the pokemon is {props.name}</p>
+        <div id={props.id} className="card">
+            <h3>{props.name}</h3>
+            <img src={imgSrc}/>
+            <p>Type: {props.type}</p>
+            <p>EXP: {props.base_experience}</p>
         </div>
     )
 }
